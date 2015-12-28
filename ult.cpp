697d659f15a7db5619da2ba94a5712f8f26ba915
wsywsya119 <wsywsya119@hotmail.com>
2015-12-28 17:47:25 +0800

    The ultrans program

---
+++ ult.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+
+
+int main()
+{
+	string input;
+
+	cout<<"Enter your sentence to transform>>"<<endl;
+	getline(cin,input);
+
+	cout<<"The original sentence: "<<input<<endl;
+
+	char r;
+	int ascii='a'-'A';
+	string output;
+
+	for(int i=0;i<input.length();i++)
+	{
+		r=input.at(i);
+
+		if('a'<=r && r<='z')
+		{
+			output.push_back(r-ascii);
+		}
+		else if('A'<=r && r<='Z')
+		{
+			output.push_back(r+ascii);
+		}
+		else
+		{
+			output.push_back(r);
+		}
+	}
+
+	cout<<output<<endl;
+
+}
